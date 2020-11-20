26ce770a965404445e245cc13265006a5dccc44c
GPCIS <UGPCIS@gmail.com>
2020-11-20 06:54:06 +0200

    Implementing the partial sums.

---
+++ Arrays/Partial Sums/Partial Sums v01.cpp
@@ -0,0 +1,33 @@
+#include "iostream"
+
+using namespace std;
+
+const int sMax = 100007;
+
+int sum[sMax];
+
+void ComputSum(int a[], int lenght)
+{
+
+    for (int i = 0; i < lenght; i++)
+    {
+        if(i = 0)
+            sum[i] = a[i];
+        else
+        {
+            sum[i] = sum[i -1] + a[i];
+        }
+        
+    }
+}
+
+long long Qury(int y, int x)
+{
+    return sum[x] - sum[y - 1];
+}
+
+
+int main()
+{
+    return 0;
+}
